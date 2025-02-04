90d00e1db9dae9eaf87b87b4e380b229853d869f
ananthee89 <ananthee89@gmail.com>
2025-02-04 10:07:27 +0530

    update in environments.ts
    update in environments.ts

--- src/app/pages/environments/environments.ts
+++ src/app/pages/environments/environments.ts
@@ -1,31 +1,31 @@
 export const environmentpath={
-  searchCustomer :'http://localhost:9201/stashook/searchCustomer',
-  addCustomer :'http://localhost:9201/stashook/addCustomer',
+  searchCustomer :'http://localhost:9200/stashook/searchCustomer',
+  addCustomer :'http://localhost:9200/stashook/addCustomer',
 
-  searchIncident : 'http://localhost:9201/stashook/searchIncident',
-  addIncident :'http://localhost:9201/stashook/createIncident',
+  searchIncident : 'http://localhost:9200/stashook/searchIncident',
+  addIncident :'http://localhost:9200/stashook/createIncident',
 
   apiUrl: 'http://localhost:9000',
   menuUrl:"http://localhost:9000/stashook/menuList",
 
-  markAttendance:"http://localhost:9200/stashook/markAttendance",
-  getMarkAttendance:"http://localhost:9200/stashook/getMarkedAttendance",
+  markAttendance:"http://localhost:9100/stashook/markAttendance",
+  getMarkAttendance:"http://localhost:9100/stashook/getMarkedAttendance",
 
-  getProjectList:"http://localhost:9200/stashook/getProjectList",
-  getProcessList:"http://localhost:9200/stashook/getProcessList",
-  getTimesheetId:"http://localhost:9200/stashook/getTimesheetByDateRange",
+  getProjectList:"http://localhost:9100/stashook/getProjectList",
+  getProcessList:"http://localhost:9100/stashook/getProcessList",
+  getTimesheetId:"http://localhost:9100/stashook/getTimesheetByDateRange",
   
-  addDailyLog:"http://localhost:9200/stashook/addUserDailyLog",
-  getDailyLog:"http://localhost:9200/stashook/getUserDailyLog",
-  deleteDailyLog:"http://localhost:9200/stashook/deleteUserDailyLog",
-  submitTimesheet:"http://localhost:9200/stashook/updateTimesheet",
+  addDailyLog:"http://localhost:9100/stashook/addUserDailyLog",
+  getDailyLog:"http://localhost:9100/stashook/getUserDailyLog",
+  deleteDailyLog:"http://localhost:9100/stashook/deleteUserDailyLog",
+  submitTimesheet:"http://localhost:9100/stashook/updateTimesheet",
 
-  applyLeave:"http://localhost:9200/stashook/applyLeave",
-  cancelLeave:"http://localhost:9200/stashook/cancelLeave",
-  getLeaveType:"http://localhost:9200/stashook/getLeaveTypeList",
-  searchLeave:"http://localhost:9200/stashook/searchLeave",
-  getLeaveHolidayColor:"http://localhost:9200/stashook/getLeaveHolidayColor",
-  getLeaveBalance:"http://localhost:9200/stashook/getLeaveBalance",
+  applyLeave:"http://localhost:9100/stashook/applyLeave",
+  cancelLeave:"http://localhost:9100/stashook/cancelLeave",
+  getLeaveType:"http://localhost:9100/stashook/getLeaveTypeList",
+  searchLeave:"http://localhost:9100/stashook/searchLeave",
+  getLeaveHolidayColor:"http://localhost:9100/stashook/getLeaveHolidayColor",
+  getLeaveBalance:"http://localhost:9100/stashook/getLeaveBalance",
 
 
 
